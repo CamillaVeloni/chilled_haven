c3f51ac6db546a9acdd7cf8544492f894da9ab3f
CamillaVeloni <Zz01594120>
2023-05-30 19:47:26 -0300

    constant

---
+++ lib/core/api/api_constants.dart
@@ -0,0 +1,12 @@
+class Constants {
+  static const baseUrl = "http://change_ip:port_here/";
+
+  static const registrationUrl = "/registration";
+  static const loginUrl = "/login";
+  static const userUrl = "/getUser";
+
+  static const productsUrl = '/products';
+
+  // ViaCEP
+  static const viaCepUrl = "https://viacep.com.br/ws/{cep}/json/";
+}
