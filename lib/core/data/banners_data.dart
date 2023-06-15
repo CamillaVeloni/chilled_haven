da2ded01c562148de4be3193330c9f72a2ef151d
CamillaVeloni <Zz01594120>
2023-06-14 21:21:28 -0300

    banner data

---
+++ lib/core/data/banners_data.dart
@@ -0,0 +1,14 @@
+const BANNERS_DATA = [
+  {
+    "title": "Ofertas fresquinhas 🍨❤",
+    "description": "Promoções imperdíveis que preparamos especialmente para você.",
+    "discount": "5%",
+    "imageUrl": ""
+  },
+  {
+    "title": "Indique e Ganhe!",
+    "description": "Para cada indicação, você ganhará 20% de desconto na sua próxima visita.",
+    "discount": "20%",
+    "imageUrl": "",
+  }
+];
